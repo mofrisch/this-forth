0eeeacf90a985d4032bad0ca2fc4f9254764267c
Moritz Frisch <mo_68@gmx.de>
2022-12-02 17:12:52 +0100

    Initial Commit

---
+++ this-forth/this_forthApp.swift
@@ -0,0 +1,17 @@
+//
+//  this_forthApp.swift
+//  this-forth
+//
+//  Created by Moritz on 02.12.22.
+//
+
+import SwiftUI
+
+@main
+struct this_forthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
